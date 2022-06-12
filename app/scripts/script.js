23cbf07cde02c29e871c06b2072070cf7dcdbe4f
Harsh Jain <harshjain5454@gmail.com>
2022-06-12 13:51:12 +0530

    :sparkles:GET advice from API and render it

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,1 +1,26 @@
-console.log("Hello");
+const adviceIdElement = document.getElementById("advice-id");
+const adviceTextElement = document.getElementById("advice-text");
+const generateAdviceButton = document.getElementById("generate-advice");
+const getAdviceFromAPI = () => {
+    const randomAdviceSlipURL = "https://api.adviceslip.com/advice";
+
+    fetch(randomAdviceSlipURL)
+    .then(response => response.json())
+    .then(adviceSlipResponse => {
+        adviceIdElement.innerText = adviceSlipResponse.slip.id;
+        adviceTextElement.innerText = adviceSlipResponse.slip.advice;
+    })
+    
+    .catch(err => {
+        adviceIdElement.innerText ="Not available!"
+        adviceTextElement.innerText = "Could not retrieve now, Please try later! Thanks"
+    });
+}
+
+window.onload = () => {
+    getAdviceFromAPI();
+}
+
+generateAdviceButton.addEventListener('click', (event) => {
+    getAdviceFromAPI();
+});
